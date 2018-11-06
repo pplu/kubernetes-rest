c60dd35b1aae7903a19687fed48ed2ed8def9d37
Jose Luis Martinez <jlmartinez@capside.com>
2018-11-07 00:59:59 +0100

    Enable SSL cert authentication. Still in the works

--- lib/Kubernetes/REST/HTTPTinyIO.pm
+++ lib/Kubernetes/REST/HTTPTinyIO.pm
@@ -1,11 +1,18 @@
 package Kubernetes::REST::HTTPTinyIO;
   use Moo;
   use HTTP::Tiny;
+  use IO::Socket::SSL;
   use Kubernetes::REST::HTTPResponse;
 
   has ua => (is => 'ro', default => sub {
     HTTP::Tiny->new(
       agent => 'Kubernetes::REST Perl Client ' . $Kubernetes::REST::VERSION,
+      SSL_options => {
+        SSL_cert_file => "$ENV{HOME}/.minikube/client.crt",
+        SSL_key_file => "$ENV{HOME}/.minikube/client.key",
+        SSL_verify_mode => SSL_VERIFY_PEER,
+        SSL_ca_file => "$ENV{HOME}/.minikube/ca.crt",
+      },
     );
   });
 
